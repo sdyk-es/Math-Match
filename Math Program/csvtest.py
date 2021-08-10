3613def7b7a2b153b5d6c1281f88d72d3d51243c
sdyk041 <sdyk041@aucklanduni.ac.nz>
2021-08-10 22:21:14 +1200

    csv sorting test

--- Math Program/csvtest.py
+++ Math Program/csvtest.py
@@ -1,9 +1,16 @@
-import csv
+import csv, operator
 
-name = input("what is your name?")
-score = input("what is your score?")
-time = input("what is your time?")
-with open('test.csv', 'a', newline = '') as f:
-    writer = csv.writer(f)
-    writer.writerow([name,score,time])
+# name = input("what is your name?")
+# score = input("what is your score?")
+# time = input("what is your time?")
+# with open('test.csv', 'a', newline = '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow([name,score,time])
+
+with open('test.csv', 'r', newline = '') as f:
+    reader = csv.reader(f)
+
+    sortedlist = sorted(reader, key=operator.itemgetter(1))
+    for line in sortedlist:
+        print(line)
     
